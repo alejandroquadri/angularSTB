10d1ec60851520118c40307593eb3f925c4569c9
Alejandro Quadri <alejandroquadri@gmail.com>
2015-11-05 18:08:00 -0300

    hasta minuto 55

--- app/js/app.js
+++ app/js/app.js
@@ -29,7 +29,7 @@ angular.module('readingList', [])
       replace: 'true',
       controller: function(){
         this.showForm = false;
-        this.book = {};
+        this.book = {genres:{}};
       },
       controllerAs: 'reviewFormCtrl',
       scope: {
